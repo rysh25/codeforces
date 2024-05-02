67e82ae2893034f08f6893be7d9d3231131c91d1
Ryohei Shiraki <rysh25@gmail.com>
2024-05-02 22:41:55 +0900

    Add Educational Codeforces Round 165 (Rated for Div. 2)

---
+++ 1969/b.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <list>
+#include <utility>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <cstring>
+// #include <cstdlib>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <stack>
+#include <tuple>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define endl '\n'
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n); i++)
+using ll = long long;
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    int l = -1;
+
+    rep(i, s.size())
+    {
+        if (s[i] == '1')
+        {
+            l = i;
+            break;
+        }
+    }
+
+    if (l == -1)
+    {
+        cout << 0 << endl;
+        return;
+    }
+
+    ll ans = 0;
+    for (int r = l + 1; r < s.size(); r++)
+    {
+        if (s[r] == '0')
+        {
+            ans += (ll)r - l + 1;
+
+            l++;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
