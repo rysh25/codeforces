7560a8cf720a6b128227e46456cb84611f93ffcf
Ryohei Shiraki <rysh25@gmail.com>
2024-05-02 22:42:12 +0900

    Add Codeforces Round 942 (Div. 2)

---
+++ 1972/c.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <list>
+#include <utility>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <cstring>
+// #include <cstdlib>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <stack>
+#include <tuple>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define endl '\n'
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n); i++)
+using ll = long long;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+
+    rep(i, n) cin >> a[i];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
